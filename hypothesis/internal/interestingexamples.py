d2cbb32da02cb24629c478733a299151290dfbd6
David R. MacIver <david@drmaciver.com>
2015-01-08 09:00:33 +0000

    Automatic generation of interesting examples

---
+++ hypothesis/internal/interestingexamples.py
@@ -0,0 +1,65 @@
+import time
+import hypothesis.searchstrategy as ss
+import random as r
+import hypothesis.internal.cover as c
+import hypothesis.internal.tracker as t
+from six.moves import xrange
+
+
+def find_interesting_examples(
+    function, descriptor, timeout=60, search_strategies=None, random=None,
+    max_examples=100,
+):
+    search_strategies = search_strategies or ss.SearchStrategies()
+    strategy = search_strategies.strategy(descriptor)
+    random = random or r.Random()
+    start = time.time()
+    collector = c.Collector()
+    tracker = t.Tracker()
+    minteresting = {}
+
+    def update_minteresting(f, example):
+        if f not in minteresting:
+            minteresting[f] = example
+            return True
+        else:
+            existing = minteresting[f]
+            if strategy.complexity(example) < strategy.complexity(existing):
+                minteresting[f] = example
+                return True
+            return False
+
+    for _ in xrange(max_examples):
+        if time.time() >= start + timeout:
+            break
+        pv = strategy.parameter.draw(r)
+        example = strategy.produce(r, pv)
+        with collector:
+            function(*example)
+        pending = []
+        for f in collector.executed_features:
+            if update_minteresting(f, example):
+                pending.append((f, example))
+
+        while pending:
+            old_pending = pending
+            pending = []
+
+            for feature, example in old_pending:
+                def still_interesting(x):
+                    with collector:
+                        function(*x)
+                    for f in collector.executed_features:
+                        if update_minteresting(f, x):
+                            pending.append((f, x))
+                    return feature in collector.executed_features
+
+                for simpler in strategy.simplify_such_that(
+                    example, still_interesting
+                ):
+                    example = simpler
+
+                if tracker.track(example) == 1:
+                    yield example
+                if time.time() >= start + timeout:
+                    break
