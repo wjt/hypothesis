850ea8c691578ea739ad88378471761f56a90c5b
David R. MacIver <david@drmaciver.com>
2015-01-08 09:00:33 +0000

    Automatic generation of interesting examples

---
+++ hypothesis/internal/cover.py
@@ -0,0 +1,25 @@
+from coverage.control import Coverage
+
+
+class Collector(object):
+    def __init__(self):
+        self.known_files = {}
+        self.executed_features = set()
+
+    def __enter__(self):
+        self.coverage = Coverage(branch=True)
+        self.coverage.start()
+        self.executed_features = set()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        coverage = self.coverage
+        self.coverage = None
+        coverage.stop()
+        coverage._harvest_data()
+        for filename, arcs in coverage.data.arcs.items():
+            fileid = self.file_id(filename)
+            for a in arcs:
+                self.executed_features.add((fileid,) + a)
+
+    def file_id(self, filename):
+        return self.known_files.setdefault(len(self.known_files) + 1)
---
+++ hypothesis/internal/test_interestingexamples.py
@@ -0,0 +1,68 @@
+import random as r
+from hypothesis.internal.interestingexamples import find_interesting_examples
+
+
+INSERTION_SORT_SIZE = 4
+
+
+def quicksort(xs):
+    xs = list(xs)
+
+    def swap(i, j):
+        xs[i], xs[j] = xs[j], xs[i]
+
+    def partition(lo, hi):
+        pivot = r.randint(lo, hi-1)
+        pivot_value = xs[pivot]
+        swap(pivot, hi)
+        store = lo
+        for i in xrange(lo, hi):
+            if xs[i] < pivot_value:
+                swap(i, store)
+                store += 1
+        swap(store, hi)
+        return store
+
+    def insertion_sort(lo, hi):
+        i = 1
+        while i <= hi:
+            j = i
+            while j > 0:
+                if xs[j-1] > xs[j]:
+                    swap(j, j-1)
+                    j -= 1
+                else:
+                    break
+            i += 1
+
+    def do_quicksort(lo, hi, depth=0):
+        if hi - lo <= 1:
+            return
+        if hi - lo == 2:
+            if xs[lo] > xs[hi]:
+                swap(lo, hi)
+            return
+        if hi - lo <= INSERTION_SORT_SIZE or depth >= 10:
+            insertion_sort(lo, hi)
+            return
+        if lo < hi:
+            p = partition(lo, hi)
+            do_quicksort(lo, p-1, depth+1)
+            do_quicksort(p+1, hi, depth+1)
+    do_quicksort(0, len(xs) - 1)
+    return xs
+
+
+def test_finds_interesting_sets():
+    examples = [
+        x[0]
+        for x in find_interesting_examples(quicksort, ([int],), timeout=1)
+    ]
+    assert len(examples) > 4
+    assert [] in examples
+    assert any(len(x) > INSERTION_SORT_SIZE for x in examples)
+    assert any(
+        len(x) > INSERTION_SORT_SIZE and sorted(x) == x for x in examples)
+    assert any(
+        len(x) > INSERTION_SORT_SIZE and
+        sorted(x, reverse=True) == x for x in examples)
